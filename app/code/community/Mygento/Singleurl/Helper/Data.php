6e4940abc682d8b504c1f441b7312ce3cc6c9124
Danila Tarasov <grohotun@gmail.com>
2016-07-08 00:27:12 +0300

    debug

--- app/code/community/Mygento/Singleurl/Helper/Data.php
+++ app/code/community/Mygento/Singleurl/Helper/Data.php
@@ -16,11 +16,12 @@ class Mygento_Singleurl_Helper_Data extends Mage_Core_Helper_Abstract {
     }
 
     public function getCategories($productId, $storeId, $justIds = false) {
-        $collection = Mage::getModel('catalog/category')->getCollection();
+        $collection = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('name')->addAttributeToSelect('url_key')->addAttributeToSelect('url_path');
         $collection->getSelect()->joinLeft(
                 array('cat' => Mage::getSingleton('core/resource')->getTableName('catalog/category_product')), 'e.entity_id = cat.category_id', array('cat_position' => 'position')
         );
-        $collection->getSelect()->where('product_id = ?', $productId);
+        $collection->addUrlRewriteToResult();
+        $collection->getSelect()->where('cat.product_id = ?', $productId);
         switch (Mage::getStoreConfig('dev/singleurl/level')) {
             case "long":
                 $collection->getSelect()->order('level DESC');
@@ -33,7 +34,6 @@ class Mygento_Singleurl_Helper_Data extends Mage_Core_Helper_Abstract {
         }
         $collection->getSelect()->order('cat_position ASC');
         $collection->getSelect()->limit(1);
-        Mage::log((string) $collection->getSelect());
         if (!$justIds) {
             return $collection;
         }
